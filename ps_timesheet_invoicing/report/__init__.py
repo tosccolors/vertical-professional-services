6391d22d7451bb3633cc0d62960e1067990f8e5b
Holger Brunn <mail@hunki-enterprises.com>
2025-03-06 10:47:11 +0100

    fixup! [RFR] #577 make ps_crm work for arbitraty OUs

--- ps_timesheet_invoicing/report/__init__.py
+++ ps_timesheet_invoicing/report/__init__.py
@@ -1,4 +1,5 @@
 from . import hr_chargeability_report
 from . import status_time_report
 from . import overtime_balance_report
-from . import crm_pipeline_actuals_report
+
+# from . import crm_pipeline_actuals_report
