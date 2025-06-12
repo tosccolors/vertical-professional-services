33a74ffa5b51d8ed611104a4a90798821407f165
Holger Brunn <mail@hunki-enterprises.com>
2025-06-12 15:46:16 +0200

    fixup! [ADD] #67 ps_timesheet_invoicing_time_control

--- ps_timesheet_invoicing_time_control/__manifest__.py
+++ ps_timesheet_invoicing_time_control/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Professional services time control",
-    "summary": "Adapt time control to profession services",
+    "summary": "Adapt time control to professional services",
     "version": "16.0.1.0.0",
     "category": "module_category_specific_industry_applications",
     "author": "The Open Source Company",
@@ -13,6 +13,9 @@
         "project_timesheet_time_control",
         "helpdesk_mgmt_timesheet",
     ],
-    "data": [],
+    "data": [
+        "views/helpdesk_ticket.xml",
+        "views/ps_time_line.xml",
+    ],
     "post_init_hook": "post_init_hook",
 }
--- ps_timesheet_invoicing_time_control/models/helpdesk_ticket.py
+++ ps_timesheet_invoicing_time_control/models/helpdesk_ticket.py
@@ -5,3 +5,18 @@ class HelpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
 
     timesheet_ids = fields.One2many(comodel_name="ps.time.line")
+
+    def button_create_task(self):
+        for this in self:
+            if this.task_id:
+                continue
+            this.task_id = self.env["project.task"].create(
+                {
+                    "name": this.name,
+                    "project_id": this.project_id.id,
+                    "user_ids": [fields.Command.set(this.user_id.ids)],
+                    "partner_id": this.partner_id.id,
+                    "planned_hours": this.planned_hours,
+                }
+            )
+            this.timesheet_ids.task_id = this.task_id
